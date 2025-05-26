6d5a490356b63270ab7db3a91c4825b33b722831
VICTOR AUGUSTO MARTINS <121348236+Victor-Aaugusto@users.noreply.github.com>
2025-05-26 15:39:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const menuHamburger = document.getElementById("nav-hamburguer");
 const btnMenuHamburguer = document.getElementById("img-menu-hamburguer");
+const menuHamburgerPc = document.getElementById("navBarPc")
+const menuHamburgerActive = document.getElementById("menu-hamburguer")
+let scrollPos = 0;
 
 btnMenuHamburguer.addEventListener("click", () => {
     if (menuHamburger.classList.contains("nav-hamburguer")) {
         menuHamburger.classList.remove("nav-hamburguer");
         menuHamburger.classList.add("nav-hamburguer-active");
+        menuHamburgerActive.classList.add("menu-hamburguer-active")
+        
     } else {
         menuHamburger.classList.remove("nav-hamburguer-active");
+        menuHamburgerActive.classList.remove("menu-hamburguer-active")
         menuHamburger.classList.add("nav-hamburguer");
     }
 });
-
 
 function scrollToTop() {
     window.scrollTo({
@@ -18,3 +23,4 @@ function scrollToTop() {
       behavior: 'smooth'
     });
   }
+
